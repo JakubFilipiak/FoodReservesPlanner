5f8bd799ba73f361e68a748a3297782f122b20cd
Jakub Filipiak <kuba.filipiak52@gmail.com>
2020-04-26 15:27:52 +0200

    Initial commit

---
+++ src/main/java/com/filipiak/jakub/foodreservesplanner/FoodReservesPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.filipiak.jakub.foodreservesplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodReservesPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodReservesPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipiak/jakub/foodreservesplanner/FoodReservesPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipiak.jakub.foodreservesplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReservesPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
